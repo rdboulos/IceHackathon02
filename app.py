bba1749c353162cfb41d37e6f6f9c6e4a50a17b9
rdboulos <89005126+rdboulos@users.noreply.github.com>
2023-09-07 15:52:17 +1000

    Update app.py

--- app.py
+++ app.py
@@ -1,65 +1,57 @@
+import openai 
 import streamlit as st
 import pandas as pd
 from pandasai import PandasAI
 from pandasai.llm.openai import OpenAI
 import matplotlib.pyplot as plt
 
-API_KEY='````sk-KUXXmJ4kF````Eblnb1JXwSg````T3BlbkFJw````E8Qbz411kQ2Q````pWww90w````'
-API_KEY_2 = API_KEY.replace('````', '')
+# pip install streamlit-chat  
+from streamlit_chat import message
+
+openai.api_key = "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"
+
+# Creating a function which will generate the calls from the API
+
+# Load CSV 
+df = pd.read_csv("C:/Users/SurajKannan/OneDrive - Decision Inc/Desktop/Innovation Project/OpenAI/garminfile/GOTOES_FIT.csv")
+st.session_state.df = df
+# Define the LLM
+def generate_response(user_input):
+    llm = OpenAI(api_token=openai.api_key)
+    pandas_ai = PandasAI(llm, conversational=True)
+    x = pandas_ai.run(st.session_state.df, prompt=user_input)
+
+    fig = plt.gcf()
+    # fig, ax = plt.subplots()
+    if fig.get_axes():
+        st.pyplot(fig)
+    st.write(x)
+    return x
 
 
-st.title("Matthew Dinham")
+st.title("ICE Chatbot Assistant")
 
-st.write("2023 UCI Cycling World Championships")
-#st.write(
-    #"Looking for an example *.csv-file?, check [here](https://gist.github.com/netj/8836201).")
+# Storing the chat
+if 'generated' not in st.session_state:
+	st.session_state['generated'] = []
 
-if "openai_key" not in st.session_state:
-    with st.form("API key"):
-        key = st.text_input("OpenAI Key", value="", type="password")
-        if st.form_submit_button("Submit"):
-            st.session_state.openai_key = key
-            st.session_state.prompt_history = []
-            st.session_state.df = None
+if 'past' not in st.session_state:
+	st.session_state['past'] = []
 
-if "openai_key" in st.session_state:
-    if st.session_state.df is None:
-        uploaded_file = st.file_uploader(
-            "Choose a CSV file. This should be in long format (one datapoint per row).",
-            type="csv",
-        )
-        if uploaded_file is not None:
-            df = pd.read_csv(uploaded_file)
-            st.session_state.df = df
-            
-            #data_frames = []
-            #data_frames.append(df)
-            #combined_df = pd.concat(data_frames, ignore_index=True)
+def get_text():
+	input_text = st.text_input("You: ","Hello, how are you?", key = "input")
+	return input_text
 
-    with st.form("Question"):
-        question = st.text_input("Question", value="", type="default")
-        submitted = st.form_submit_button("Submit")
-        if submitted:
-            with st.spinner():
-                llm = OpenAI(api_token=API_KEY_2)
-                pandas_ai = PandasAI(llm, conversational=True, enable_cache=False)
-                x = pandas_ai.run(st.session_state.df, prompt=question)
-                
-                fig = plt.gcf()
-                #fig, ax = plt.subplots()
-                if fig.get_axes():
-                    st.pyplot(fig)
-                st.write(x)
-                st.session_state.prompt_history.append(question)
+user_input = get_text()
 
-    if st.session_state.df is not None:
-        st.subheader("Current dataframe:")
-        st.write(st.session_state.df)
+if user_input:
+	output = generate_response(user_input)
+	#store the output
+	st.session_state.past.append(user_input)
+	st.session_state.generated.append(output)
 
-    st.subheader("Prompt history:")
-    st.write(st.session_state.prompt_history)
+if st.session_state['generated']:
 
-
-if st.button("Clear"):
-    st.session_state.prompt_history = []
-    st.session_state.df = None
+	for i in range(len(st.session_state['generated'])-1,-1,-1):
+		message(st.session_state["generated"][i],key=str(i))
+		message(st.session_state['past'][i],is_user=True,key=str(i) + '_user')
